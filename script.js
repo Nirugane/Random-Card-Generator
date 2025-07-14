52a03d4f8dc499c9d691ab3a21b29b9adb904828
Nico <nherresk@gmail.com>
2025-07-14 13:26:58 -0500

    Add button to generate new random card

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-window.onload = () => {
-    document.querySelector('.card').classList.add(generateRandomSuit());
-    document.querySelector('.number').textContent = generateRandomNumber();
-
-};
+const card = document.querySelector('.card');
+const number = document.querySelector('.number');
+const button = document.getElementById('new-card');
 
 const generateRandomNumber = () => {
-    const numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" ];
+    const numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
     const resultNumbers = Math.floor(Math.random() * numbers.length);
-    
+
     return numbers[resultNumbers];
 };
 
 const generateRandomSuit = () => {
 
-    const suit = ["diamond", "spade", "heart", "club"]; 
+    const suit = ["diamond", "spade", "heart", "club"];
     const resultSuit = Math.floor(Math.random() * suit.length);
     return suit[resultSuit];
 };
+
+const generateNewCard = () => {
+    card.classList.remove('spade', 'heart', 'diamond', 'club');
+    card.classList.add(generateRandomSuit());
+    number.textContent = generateRandomNumber();
+};
+
+window.onload = generateNewCard;
+
+button.addEventListener('click', generateNewCard);
